e295bd77c350fb75560ce27ca87fa8bab937f494
Martiniano <Martiniano@DESKTOP-6M1FHIC>
2017-07-30 19:56:42 -0300

    Corregido error donde se mostraban elementos repetidos al "Consultar Cuentas"

--- src/main/java/viewmodels/ConsultarCuentasViewModel.java
+++ src/main/java/viewmodels/ConsultarCuentasViewModel.java
@@ -1,9 +1,11 @@
 package viewmodels;
+
 import dominio.*;
 import dominio.indicadores.ArchivoIndicadores;
 import dominio.indicadores.Indicador;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.uqbar.commons.utils.Dependencies;
@@ -12,42 +14,56 @@ import org.uqbar.commons.utils.Observable;
 @Observable
 public class ConsultarCuentasViewModel {
 	private ArrayList<Empresa> empresas;
-	private Empresa empresaSeleccionada; //Guarda la empresa seleccionada en el Selector
-	
+	private Empresa empresaSeleccionada; // Guarda la empresa seleccionada en el Selector
+
 	private static ConsultarCuentasViewModel singleton = new ConsultarCuentasViewModel();
-	
-	public static ConsultarCuentasViewModel getInstance(){
+
+	public static ConsultarCuentasViewModel getInstance() {
 		return singleton;
 	}
-	
+
 	public ArrayList<Empresa> getEmpresas() {
 		return empresas;
 	}
-	
-	public void setEmpresas(ArrayList<Empresa> empresas){
+
+	public void setEmpresas(ArrayList<Empresa> empresas) {
 		this.empresas = empresas;
 	}
-	
+
 	public Empresa getEmpresaSeleccionada() {
 		return empresaSeleccionada;
 	}
-	
+
 	public void setEmpresaSeleccionada(Empresa empresaSeleccionada) {
 		this.empresaSeleccionada = empresaSeleccionada;
 	}
 	
-	@Dependencies({"empresaSeleccionada"})
-	public ArrayList<Cuenta> getCuentasEmpresa(){//cuentasEmpresa se muestra en la tabla
+	ArrayList<Cuenta> obtenerCuentasSinRepetidos(ArrayList<Cuenta> cuentasSinRepetidos){
+		for (int i = 0; i < cuentasSinRepetidos.size(); i++) {
+			for (int j = i + 1; j < cuentasSinRepetidos.size(); j++) {
+				if(cuentasSinRepetidos.get(i).getAnio().equals(cuentasSinRepetidos.get(j).getAnio())
+						&& cuentasSinRepetidos.get(i).getTipoCuenta().equals(cuentasSinRepetidos.get(j).getTipoCuenta())
+						&& cuentasSinRepetidos.get(i).getValor() == cuentasSinRepetidos.get(j).getValor()) {
+					cuentasSinRepetidos.remove(cuentasSinRepetidos.get(i));
+					j--;
+				}
+			}
+		}
+		return cuentasSinRepetidos;
+	}
+
+	@Dependencies({ "empresaSeleccionada" })
+	public ArrayList<Cuenta> getCuentasEmpresa() {// cuentasEmpresa se muestra en la tabla
 		ArrayList<Cuenta> cuentasSeleccionadas;
-		if (empresaSeleccionada==null){
+		if (empresaSeleccionada == null) {
 			cuentasSeleccionadas = null;
 		} else {
 			cuentasSeleccionadas = this.empresaSeleccionada.getCuentas();
 			Set<Indicador> indicadoresAplicables = ArchivoIndicadores.getInstance().todosLosIndicadoresAplicablesA(empresaSeleccionada);
-			cuentasSeleccionadas.addAll(empresaSeleccionada.resultadosIndicadoresTotales(indicadoresAplicables));
+			cuentasSeleccionadas.addAll(empresaSeleccionada.resultadosIndicadoresTotales(indicadoresAplicables));			
+			cuentasSeleccionadas = obtenerCuentasSinRepetidos(cuentasSeleccionadas);
 		}
-		
 		return cuentasSeleccionadas;
 	}
-	
+
 }
