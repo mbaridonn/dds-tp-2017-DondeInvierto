f3609ecf630c69de79429d3dc55cfe3c87716a29
mbaridonn <mbaridonn@gmail.com>
2017-10-01 16:40:10 -0300

    Actualizadas dependencias y agregado Server

---
+++ src/main/java/server/Server.java
@@ -0,0 +1,13 @@
+package server;
+
+import spark.Spark;
+import spark.debug.DebugScreen;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Spark.port(8080);
+		DebugScreen.enableDebugScreen();
+	}
+
+}
