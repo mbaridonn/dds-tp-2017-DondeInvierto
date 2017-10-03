df3e4a40ca714c50e7659e9f9d1e70e7167cf0d3
martingsalazar <martin.salazar20@gmail.com>
2017-10-03 15:55:43 -0300

    Prueba de listado de cuentas con spark.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,8 +6,10 @@ import spark.debug.DebugScreen;
 public class Server {
 
 	public static void main(String[] args) {
+		new Bootstrap().init();
 		Spark.port(8080);
 		DebugScreen.enableDebugScreen();
+		Router.configure();
 	}
 
 }
